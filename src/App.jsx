9ce17b938de01afd0dab9d90bbd471a04b650584
Abhay Gupta <abhay19021@gmail.com>
2022-02-09 14:04:31 +0530

    ADD: detail page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css'; 
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import { BrowserRouter as Router, Route, Link,Routes } from "react-router-dom";
+
+function App() {
+  return (
+      <div className="App">
+          <Router>
+              <Header />
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/detail" element={<Detail/>}/>
+              </Routes>
+          </Router>
+      </div>
+  );
+}
+
+export default App;
